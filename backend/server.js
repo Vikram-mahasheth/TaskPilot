956295f4a1fe86e6af6367988229ffbc454c7940
Vikram-mahasheth <vikrammahaseth142@gmail.com>
2025-07-29 01:51:14 +0530

    add new changes

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ const __dirname = path.dirname(__filename);
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://task-pilot-rouge.vercel.app' // Add your frontend's live URL here
+  'https://task-pilot-psi.vercel.app/' // Add your frontend's live URL here
 ];
 
 app.use(cors({
